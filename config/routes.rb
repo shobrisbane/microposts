c0fc419988dfcf77513a3e65a1ce917f2e5a631c
shobrisbane <shobrisbane@hotmail.com>
2017-03-12 11:39:14 +0000

    add sign sign up and user detail

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,9 @@ Rails.application.routes.draw do
   post   'login' , to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
 
-
   resources :users
+
+  root 'messages#index'
+  resources :messages , except: [:index, :new]
+  
 end
