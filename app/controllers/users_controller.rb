080397a437f352dec2872b2c07f6d0b5b4f9fa2a
shobrisbane <shobrisbane@hotmail.com>
2017-03-12 10:39:58 +0000

    twitter

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,17 @@
 class UsersController < ApplicationController
   
-    def new
+  def show # 追加
+   @user = User.find(params[:id])
+  end
+  
+  def new
     @user = User.new
-   
-    end
+  end
     
   def create
     @user = User.new(user_params)
     if @user.save
+      redirect_to @user # ここを修正    
     else
       render 'new'
     end
